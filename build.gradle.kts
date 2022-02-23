2307249a00bdc1af6f7c768ac1c89b7ce40b0867
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2022-02-23 14:44:45 +0100

    created plugin for project build named blackjack.kotlin-conventions
    - plugin definition is fully type safe compatible
    - moved configuration for task test and test listener to buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,8 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("com.github.ben-manes.versions") version "0.42.0" apply false
 }
 
 subprojects {
-    apply(plugin = "kotlin-conventions")
+    apply(plugin = "blackjack.kotlin-conventions")
     apply(plugin = "com.github.ben-manes.versions")
-
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-        mavenLocal()
-    }
-
-    dependencies {
-        val testImplementation by configurations
-        testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
-    }
-
-    tasks {
-        named("compileKotlin", KotlinCompile::class) {
-            kotlinOptions {
-                allWarningsAsErrors = true
-            }
-        }
-    }
 }
