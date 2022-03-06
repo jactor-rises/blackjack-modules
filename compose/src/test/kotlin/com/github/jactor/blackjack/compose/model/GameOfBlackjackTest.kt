0c936eea101be674f68b43a06877c6a1501d5363
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2022-03-06 11:32:39 +0100

    displays different winner text when blackjack

--- compose/src/test/kotlin/com/github/jactor/blackjack/compose/model/GameOfBlackjackTest.kt
+++ compose/src/test/kotlin/com/github/jactor/blackjack/compose/model/GameOfBlackjackTest.kt
@@ -1,7 +1,9 @@
 package com.github.jactor.blackjack.compose.model
 
 import com.github.jactor.blackjack.dto.GameOfBlackjackDto
+import com.github.jactor.blackjack.dto.GameStatus
 import com.github.jactor.blackjack.dto.GameType
+import com.github.jactor.blackjack.dto.StatusDto
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatIllegalStateException
 import org.junit.jupiter.api.DisplayName
@@ -35,5 +37,39 @@ internal class GameOfBlackjackTest {
             ).with(PlayerName("jactor"))
         }
             .withMessage("The nick of added player do not match the game nick (turbo)!")
+    }
+
+    @Test
+    fun `should display winner`() {
+        val gameOfBlackjack = GameOfBlackjack(
+            GameOfBlackjackDto(
+                nickOfPlayer = "tor-egil",
+                status = StatusDto(
+                    result = GameStatus.PLAYER_WINS,
+                    playerScore = 20,
+                    dealerScore = 22,
+                    isGameCompleted = true
+                )
+            )
+        ).with(PlayerName("tor egil"))
+
+        assertThat(gameOfBlackjack.displayWinner()).isEqualTo("Tor Egil er vinneren!")
+    }
+
+    @Test
+    fun `should display blackjack`() {
+        val gameOfBlackjack = GameOfBlackjack(
+            GameOfBlackjackDto(
+                nickOfPlayer = "tor-egil",
+                status = StatusDto(
+                    result = GameStatus.PLAYER_WINS,
+                    playerScore = 21,
+                    dealerScore = 22,
+                    isGameCompleted = true
+                )
+            )
+        ).with(PlayerName("tor egil"))
+
+        assertThat(gameOfBlackjack.displayWinner()).isEqualTo("Tor Egil fikk blackjack!")
     }
 }
