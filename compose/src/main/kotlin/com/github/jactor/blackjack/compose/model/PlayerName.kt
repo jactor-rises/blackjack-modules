0cf03c4ef8da5788c91524f2fbcc1da2ac90c014
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2022-03-08 09:59:05 +0100

    Nick of player is not automatically derived from PlayerName.name

--- compose/src/main/kotlin/com/github/jactor/blackjack/compose/model/PlayerName.kt
+++ compose/src/main/kotlin/com/github/jactor/blackjack/compose/model/PlayerName.kt
@@ -1,10 +1,14 @@
 package com.github.jactor.blackjack.compose.model
 
-data class PlayerName(private val name: String) {
+import com.github.jactor.blackjack.compose.Constants
+
+data class PlayerName(private val name: String = Constants.DEFAULT_PLAYER_NAME, val nick: String = Constants.DEFAULT_PLAYER_NICK) {
     val capitalized: String
         get() = name.split(" ").toList().joinToString(separator = " ") { string ->
             string.replaceFirstChar { if (it.isLowerCase()) it.titlecase(java.util.Locale.getDefault()) else it.toString() }
         }
 
-    val nick: String get() = name.replace(" ", "-").lowercase()
+    companion object {
+        fun asLowerCaseAndDashAsSpace(name: String): String = name.replace(" ", "-").lowercase()
+    }
 }
